eebb7ae28e3bad3656cb2b9e5d4c13a289039635
Suman Kumar <kumar_sn@India.XoriantCorp.com>
2021-01-14 12:43:10 +0530

    test

--- codes/create_tables.py
+++ codes/create_tables.py
@@ -6,9 +6,5 @@ cursor = connection.cursor()
 create_users_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_users_table)
 
-# create_items_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
-# cursor.execute(create_items_table)
-# cursor.execute("INSERT INTO items VALUES ('test1', 19.99)")
-
 connection.commit()
 connection.close()
